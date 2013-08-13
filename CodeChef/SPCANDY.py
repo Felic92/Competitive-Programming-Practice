865c46766e244e2c0ad90a780d15c8c08ac80ca5
Michael Feliciano <mefelici@g.cofc.edu>
2013-08-13 01:08:59 -0400

    2/1000

---
+++ CodeChef/SPCANDY.py
@@ -0,0 +1,22 @@
+#August Long 2013
+#http://www.codechef.com/AUG13/problems/SPCANDY/
+
+cases = int(raw_input())
+
+for i in range(cases):
+    
+    n,k = map(int,raw_input().split())
+
+    if(n == 0):
+        stud = 0
+        rem = 0
+    elif(k == 0):
+        stud = 0
+        rem = n
+    else:
+        stud = n//k
+        rem = n%k
+        
+    print str(stud)+" "+ str(rem)
+
+    
