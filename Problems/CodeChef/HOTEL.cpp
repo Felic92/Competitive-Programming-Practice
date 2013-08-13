9fe92d21da449f3953374895636ae39e37ba8e4f
Michael Feliciano <mefelici@g.cofc.edu>
2013-08-13 14:00:01 -0400

    18/1000

---
+++ Problems/CodeChef/HOTEL.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    int tc,guests,i,j,k;
+
+
+
+    scanf("%d",&tc);
+
+    while(tc--)
+    {
+        int simulate [1005] = {0};
+        int a,d;
+        scanf("%d",&guests);
+
+        for(i = 0; i < guests; i++)
+        {
+            scanf("%d",&a);
+            simulate[a]+=1;
+        }
+
+        for(j = 0; j < guests; j++)
+        {
+            scanf("%d",&d);
+            simulate[d]-=1;
+        }
+
+        int max = 0;
+        int current = 0;
+        for(k=1;k<1001;k++)
+        {
+            current += simulate[k];
+            if(current > max)
+            {
+                max = current;
+            }
+        }
+
+        printf("%d\n",max);
+
+    }
+
+    return 0;
+}
