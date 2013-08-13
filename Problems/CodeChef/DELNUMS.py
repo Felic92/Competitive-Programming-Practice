0d839b1c40d8e67d1d51506245c99ff8f49650d3
Michael Feliciano <mefelici@g.cofc.edu>
2013-08-13 01:30:36 -0400

    5/1000

---
+++ Problems/CodeChef/DELNUMS.py
@@ -0,0 +1,14 @@
+#August Long 2013
+#Deleting Numbers
+#http://www.codechef.com/AUG13/problems/DELNMS/
+
+def main():
+ 
+    length = int(raw_input())
+    values = raw_input()
+ 
+    print length
+    for i in range(length):
+        print length-i, length-i
+            
+main() 
