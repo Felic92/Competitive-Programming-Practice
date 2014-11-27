5f2663b4dbe44fa3b781244db38e7658847c763b
Michael Feliciano <mfeliciano.enrique@gmail.com>
2014-11-26 20:17:18 -0500

    Commented CandyBoxes problem

--- Problems/CodeForces/2014/B_CandyBoxes_488.py
+++ Problems/CodeForces/2014/B_CandyBoxes_488.py
@@ -1,4 +1,24 @@
-from __future__ import division
+# Problem: B Candy Boxes
+# Author: Michael Feliciano
+# Goal: Given n integers ( n <= 4) can you generate 4 - n other integers
+# such that the mean == range == median
+
+# Approach: We take different approaches depending on the number n
+# if n is 0 we can output a sequence that we know holds this property [1,1,3,3]
+# if n is 1 we can expand the example we used when n was to and return [n,3n,3n]# if n is 2 we have various options one being to enumerate all options and check# each time since we can restrict pairs we have to check. I tried this and it 
+# got TLE in python I saw an example of it working in Pascal though. There is
+# also an algebraic solution for the n == 2 case:  {x, y, 4x - y, 3x}.
+
+# if  n is 3 you can sucessfully bruteforce check the 3 given values along
+# with all values 1 - 500 as I do below. 
+
+# if n is 4 you can simply check if the conditions hold on the 4 numbers given
+
+# Difficulty: Medium
+# Tags: Math, Brute Force 
+# Problem Link: http://codeforces.com/contest/488/problem/B
+
+from __future__ import division  #Allows for normal division
 
 def main():
 	
@@ -35,7 +55,6 @@ def solve(numCandies,cList):
 		if(3*x < y):
 			result.append("NO")
 		else:
-			#Yarr! Here be sorcery!
 			result = ["YES", 4*x-y, 3*x]
 	
 	elif(numCandies == 3):
